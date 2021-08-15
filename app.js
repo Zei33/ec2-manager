6c6edf09c4714c0a075ddc798c6e29453e10a8fe
Matthew Scott <zei@zeiworld.net>
2021-08-15 17:34:53 +1000

    Added check to ensure state isn't changed while transitioning.

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ AWS.config.update(new AWS.Config({
 
 const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 
+function cancel(){
+	console.log(`Thank you for using EC2 manager.`); 
+	process.exit(0);
+};
+
 console.log(`EC2 Instance Manager by Matthew Scott`);
 async function main(){
 	console.log(`Listing all instances.`);
@@ -39,15 +44,18 @@ async function main(){
 		validate: input => parseInt(input) >= 0 && parseInt(input) <= instances.length - 1 ? true : 'Invalid instance number.'
 	});
 	
-	var cancel = () => {
-		console.log(`Thank you for using EC2 manager.`); 
-		process.exit(0);
-	};
 	var selected = instances[+selection.selected] ?? cancel(); 
 	
 	console.log(`Selected ${selected.Tags.find(x => x.Key == 'Name').Value} (${selected.InstanceId}).`);
 	
-	if (selected.State.Code == 80){
+	const currentState = await new Promise((resolve, reject) => ec2.describeInstanceStatus({ InstanceIds: [ selected.InstanceId ], IncludeAllInstances: true }, (error, data) => error ? reject(error) : resolve(data.InstanceStatuses?.[0]?.InstanceState.Code)))
+	.catch(error => {
+		console.log(`ERROR: An error occurred when checking instance state.`);
+		console.log(`ERROR: ${error}`);
+		process.exit(0);
+	});
+	
+	if (currentState == 80){
 		await new Promise((resolve, reject) => {
 			ec2.startInstances({ InstanceIds: [ selected.InstanceId ] }, (error, data) => {
 				if (error) reject(error);
@@ -60,7 +68,7 @@ async function main(){
 			console.log(`ERROR: ${error}`);
 			process.exit(0);
 		});
-	}else{
+	}else if (currentState == 16){
 		await new Promise((resolve, reject) => {
 			ec2.stopInstances({ InstanceIds: [ selected.InstanceId ] }, (error, data) => {
 				if (error) reject(error);
@@ -73,6 +81,9 @@ async function main(){
 			console.log(`ERROR: ${error}`);
 			process.exit(0);
 		});
+	}else{
+		console.log(`This instance is currently in a state of transition and can't be modified right now.`);
+		return main();
 	}
 	
 	await new Promise((resolve, reject) => {
@@ -81,8 +92,8 @@ async function main(){
 			ec2.describeInstanceStatus({ InstanceIds: [ selected.InstanceId ], IncludeAllInstances: true }, (error, data) => {
 				if (error) reject(error);
 				
-				if ((selected.State.Code == 80 && (data.InstanceStatuses?.[0]?.InstanceState.Code ?? -1) == 16) || 
-					(selected.State.Code == 16 && (data.InstanceStatuses?.[0]?.InstanceState.Code ?? -1) == 80)) resolve(data.InstanceStatuses[0].InstanceState.Name);
+				if ((currentState == 80 && (data.InstanceStatuses?.[0]?.InstanceState.Code ?? -1) == 16) || 
+					(currentState == 16 && (data.InstanceStatuses?.[0]?.InstanceState.Code ?? -1) == 80)) resolve(data.InstanceStatuses[0].InstanceState.Name);
 			});
 		}, 1000);
 	})
@@ -98,9 +109,6 @@ async function main(){
 	main();
 }
 
-process.on('SIGINT', () => {
-	console.log(`Thank you for using EC2 manager.`);
-	process.exit(0);
-});
+process.on('SIGINT', () => cancel());
 
 main();
